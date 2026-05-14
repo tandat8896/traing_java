01344441d4f251b19ea1b3c563e2b1b0cd98637d
Tan Dat <tandat88963820@gmail.com>
2026-05-14 08:25:34 +0700

    Chrone:add feature 06

--- Promise/06_database.js
+++ Promise/06_database.js
@@ -1,40 +1,37 @@
-require("dotenv").config();
+     require("dotenv").config();
 
 const { Client } = require("pg");
 
-const client = new Client({
+console.log("ENV CHECK:", {
   host: process.env.PGHOST,
   port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  db: process.env.PGDATABASE,
+});
+
+const client = new Client({
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT), 
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
 });
 
-client.connect()
-
+client
+  .connect()
   .then(() => {
-
     console.log("Connected to PostgreSQL!");
 
     return client.query("SELECT * FROM users");
   })
-
   .then((result) => {
-
     console.log("Users table:");
     console.log(result.rows);
-
   })
-
   .catch((err) => {
-
-    console.log("Error:");
-    console.log(err);
-
+    console.error("Error:");
+    console.error(err);
   })
-
   .finally(() => {
-
     client.end();
-
   });
